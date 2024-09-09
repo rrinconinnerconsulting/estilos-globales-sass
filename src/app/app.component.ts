1d03e8d19ee34a15685bca95c2fefbbafe1b9e77
rrinconinnerconsulting <rrincon@innerconsulting.com>
2024-09-09 15:36:14 -0500

    se suben cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { NavarComponent } from './navar/navar.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/navar/navar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navar.component.html',
+  styleUrl: './navar.component.scss'
+})
+export class NavarComponent {
+
+}
